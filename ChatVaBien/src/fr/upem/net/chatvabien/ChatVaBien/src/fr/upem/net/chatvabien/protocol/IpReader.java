0c068c03ed1a35f9e2c1fbcc1425a0ccf3c82779
Rayan GRIB <rayan.grib2@edu.esiee.fr>
2025-05-14 13:38:28 +0000

    Téléverser un nouveau fichier

---
+++ ChatVaBien/src/fr/upem/net/chatvabien/ChatVaBien/src/fr/upem/net/chatvabien/protocol/IpReader.java
@@ -0,0 +1,80 @@
+package fr.upem.net.chatvabien.protocol;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
+public class IpReader implements Reader<InetAddress> {
+
+    private enum State { WAITING_TYPE, WAITING_ADDRESS, DONE, ERROR }
+
+    private State state = State.WAITING_TYPE;
+    private byte ipType;
+    private ByteBuffer ipBuffer;
+    private InetAddress ip;
+
+    @Override
+    public ProcessStatus process(ByteBuffer bb) {
+        if (state == State.DONE || state == State.ERROR) {
+            throw new IllegalStateException();
+        }
+
+        switch (state) {
+            case WAITING_TYPE -> {
+                if (bb.remaining() < 1) {
+                    return ProcessStatus.REFILL;
+                }
+                ipType = bb.get();
+                if (ipType == 0x04) {
+                    ipBuffer = ByteBuffer.allocate(4);
+                } else if (ipType == 0x06) {
+                    ipBuffer = ByteBuffer.allocate(16);
+                } else {
+                    state = State.ERROR;
+                    return ProcessStatus.ERROR;
+                }
+                //System.out.println("ipType: " + ipType);
+                //System.out.println("ipBuffer remaining: " + ipBuffer.remaining());
+                //System.out.println("Byte read: " + Arrays.toString(ipBuffer.array()));
+                state = State.WAITING_ADDRESS;
+                return ProcessStatus.REFILL;
+            }
+
+            case WAITING_ADDRESS -> {
+                int toRead = Math.min(bb.remaining(), ipBuffer.remaining());
+                for (int i = 0; i < toRead; i++) {
+                    ipBuffer.put(bb.get());
+                }
+                if (ipBuffer.hasRemaining()) {
+                    return ProcessStatus.REFILL;
+                }
+                try {
+                    ip = InetAddress.getByAddress(ipBuffer.array());
+                } catch (UnknownHostException e) {
+                    state = State.ERROR;
+                    return ProcessStatus.ERROR;
+                }
+                state = State.DONE;
+                return ProcessStatus.DONE;
+            }
+
+            default -> throw new IllegalStateException();
+        }
+    }
+
+    @Override
+    public InetAddress get() {
+        if (state != State.DONE) {
+            throw new IllegalStateException();
+        }
+        return ip;
+    }
+
+    @Override
+    public void reset() {
+        state = State.WAITING_TYPE;
+        ipBuffer = null;
+        ip = null;
+    }
+}
