b1e7fc4a778a157ddeef2607eda4c51c7f8ef262
Rayan GRIB <rayan.grib2@edu.esiee.fr>
2025-05-14 13:37:23 +0000

    Téléverser un nouveau fichier

---
+++ ChatVaBien/src/fr/upem/net/chatvabien/ChatVaBien/src/fr/upem/net/chatvabien/protocol/StringReader.java
@@ -0,0 +1,82 @@
+package fr.upem.net.chatvabien.protocol;
+
+
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class StringReader implements Reader<String> {
+
+    private enum State {WAITING_SIZE, WAITING_STRING, DONE, ERROR}
+
+    private final Charset UTF8 = StandardCharsets.UTF_8;
+    private final IntReader intReader = new IntReader();
+    private State state = State.WAITING_SIZE;
+    private int size;
+    private ByteBuffer internalBuffer;
+    private String value;
+
+    @Override
+    public ProcessStatus process(ByteBuffer bb) {
+        if (state == State.DONE || state == State.ERROR) {
+            throw new IllegalStateException();
+        }
+
+        if (state == State.WAITING_SIZE) {
+            var status = intReader.process(bb);
+            if (status != ProcessStatus.DONE) {
+                return status;
+            }
+
+            size = intReader.get();
+            intReader.reset();
+
+            if (size < 0 || size > 1024) {
+                state = State.ERROR;
+                return ProcessStatus.ERROR;
+            }
+
+            internalBuffer = ByteBuffer.allocate(size);
+            state = State.WAITING_STRING;
+        }
+
+        if (state == State.WAITING_STRING) {
+            bb.flip();
+            try {
+                int toRead = Math.min(bb.remaining(), internalBuffer.remaining());
+                for (int i = 0; i < toRead; i++) {
+                    internalBuffer.put(bb.get());
+                }
+            } finally {
+                bb.compact();
+            }
+
+            if (internalBuffer.hasRemaining()) {
+                return ProcessStatus.REFILL;
+            }
+
+            internalBuffer.flip();
+            value = UTF8.decode(internalBuffer).toString();
+            state = State.DONE;
+            return ProcessStatus.DONE;
+        }
+
+        return ProcessStatus.ERROR;
+    }
+
+    @Override
+    public String get() {
+        if (state != State.DONE) {
+            throw new IllegalStateException();
+        }
+        return value;
+    }
+
+    @Override
+    public void reset() {
+        state = State.WAITING_SIZE;
+        intReader.reset();
+        internalBuffer = null;
+        value = null;
+    }
+}
